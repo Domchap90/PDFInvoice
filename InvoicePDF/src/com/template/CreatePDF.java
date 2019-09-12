a779afa63e48ac8731ffb83c71cc54cce76fc1e2
Domchap90 <domchaple@gmail.com>
2019-09-12 15:46:41 +0100

    Table completed with formatting and totals

--- InvoicePDF/src/com/template/CreatePDF.java
+++ InvoicePDF/src/com/template/CreatePDF.java
@@ -3,18 +3,27 @@ package com.template;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class CreatePDF {
+
+	public static String formatToCurrency(BigDecimal bd) {
+		String currencyFormat = NumberFormat.getCurrencyInstance().format(bd);
+		return currencyFormat;
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO upload picture (logo) top centre
@@ -22,12 +31,12 @@ public class CreatePDF {
 		// Description
 		// Amount
 		Document doc = new Document ();
-		String fileName = "Deposit_fund_receipt.pdf";
+		String fileName = "Invoice.pdf";
 		try {
 			PdfWriter writer = PdfWriter.getInstance (doc, new FileOutputStream(fileName));
 			doc.open();
 
-			String imageFile = "logo.png";
+			String imageFile = "Logo.png";
 			Image image = Image.getInstance(imageFile);
 			image.scaleAbsolute(125, 125);
 			image.setAbsolutePosition(225, 700);
@@ -35,7 +44,7 @@ public class CreatePDF {
 
 			// Text title = new Text("Deposit fund receipt");
 			Font font = new Font(FontFamily.TIMES_ROMAN, 20.0f);
-			Paragraph title = new Paragraph("Deposit fund receipt", font);
+			Paragraph title = new Paragraph("Invoice", font);
 			title.setSpacingBefore(200);
 			title.setIndentationLeft(200);
 			doc.add(title);
@@ -46,16 +55,36 @@ public class CreatePDF {
 			table.setSpacingAfter(11f);
 			float[] colWidth = { 2f, 2f, 2f, 2f };
 			table.setWidths(colWidth);
-			// create cells
-			PdfPCell c1 = new PdfPCell(new Paragraph("Amount"));
-			PdfPCell c2 = new PdfPCell(new Paragraph(""));
-			PdfPCell c3 = new PdfPCell(new Paragraph(""));
-			PdfPCell c4 = new PdfPCell(new Paragraph("100.00"));
-			// add cells to table
-			table.addCell(c1);
-			table.addCell(c2);
-			table.addCell(c3);
-			table.addCell(c4);
+
+			// Create content of cells to be added to table.
+			PdfPCell cell = new PdfPCell();
+
+			int productNum = 0;
+			BigDecimal sum = new BigDecimal(0);
+			for (int c = 0; c < 24; c++) {
+				if (c % 4 == 0 && c < 12) {
+					productNum = (c / 4) + 1;
+					cell.setPhrase(new Phrase(new Product(productNum).getProduct()));
+				} else if (c % 4 == 3 && c < 12) {
+					BigDecimal price = new Product(productNum).getPrice();
+					cell.setPhrase(new Phrase(formatToCurrency(price)));
+					sum = sum.add(price);
+				} else if (c == 18) {
+					cell.setPhrase(new Phrase("Subtotal\n + VAT"));
+				} else if (c == 19) {
+					cell.setPhrase(new Phrase(formatToCurrency(sum)));
+				} else if (c == 22) {
+					cell.setPhrase(new Phrase("Total"));
+				} else if (c == 23) {
+					sum = sum.multiply(new BigDecimal(1.20));
+					cell.setPhrase(new Phrase(formatToCurrency(sum)));
+				} else {
+					cell.setPhrase(new Phrase(" "));
+				}
+				cell.disableBorderSide(15);
+				cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+				table.addCell(cell);
+				}
 			doc.add(table);
 			// add table to doc
 			doc.close();
--- InvoicePDF/src/com/template/Product.java
+++ InvoicePDF/src/com/template/Product.java
@@ -1,9 +1,25 @@
 package com.template;
 
-public class InvoiceObject {
-	// Description
-	// Amount
-	// Deposit fund Source
-	// Name + username
+import java.math.BigDecimal;
+
+public class Product {
+
+	private String[] products = { "Package of 3 ", "Package of 10", "Package of 20" };
+	private BigDecimal[] prices = { BigDecimal.valueOf(210.00), BigDecimal.valueOf(600.00),
+			BigDecimal.valueOf(1100.00) };
+	private int option;
+
+	Product(int option) { // chose one of three products
+		this.option = option;
+	}
+
+	public BigDecimal getPrice() {// chose 1 through 3
+		BigDecimal bd = prices[--option];
+		return bd;
+	}
+
+	public String getProduct() {// chose 1 through 3
+		return products[--option];
+	}
 
 }
